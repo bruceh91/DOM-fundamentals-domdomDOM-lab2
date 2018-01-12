cc2c37b19d84a3fccd4d76c195d06e681b0e7494
Bruce Harris <bruceharris91@gmail.com>
2018-01-11 23:38:20 -0600

    changes

--- scripts.js
+++ scripts.js
@@ -5,14 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
     let btnText = document.createTextNode('Add Square');
     button.appendChild(btnText);
     document.body.appendChild(button);
-    
+
     // used in makeBox and giveId for generation id
     count = 1;
-
+    temp = 
     //used to generate box with unique id when clicked
     button.addEventListener("click", function () {
         makeBox();
         count++;
+        giveColor(temp);
     })
 
     //generate box with unique id
@@ -20,33 +21,48 @@ document.addEventListener("DOMContentLoaded", function () {
         let div = document.createElement('div');
         div.className = "black-box";
         div.id = count;
+        temp = div;
         document.body.appendChild(div);
         giveNum(div);
-       
+        
     }
 
     // puts number in square only when hovered on
     function giveNum(div) {
-        
-    // displays number
- div.addEventListener("mouseover", function () {       
-    let p = document.createElement('p');
-    let boxText = document.createTextNode(div.id);
-    p.appendChild(boxText)
-    div.appendChild(p)
 
-    //hides number
-    div.addEventListener("mouseout", function (){    
-        div.removeChild(p);     // <= cosole shows this error   
-                                //Uncaught DOMException: Failed to execute 'removeChild' on 'Node': 
-                                //The node to be removed is not a child of this node.
-    })
-    })
+        // displays number
+        div.addEventListener("mouseover", function () {
+            let p = document.createElement('p');
+            let boxText = document.createTextNode(div.id);
+            p.appendChild(boxText);
+            div.appendChild(p);
+
+            //hides number
+            div.addEventListener("mouseout", function () {
+                div.removeChild(p);     // <= cosole shows this error   
+                //Uncaught DOMException: Failed to execute 'removeChild' on 'Node': 
+                //The node to be removed is not a child of this node.
+            })
+        })
+    }
+
+    function giveColor (div) {
+        div.addEventListener("click", function() {
+            let r = genNums();
+            let g = genNums();
+            let b = genNums();
+            let colorGen = "rgb_number= 'rgb(" + r + "," + g + "," + b + ")';";
+            div.setAttribute('class', colorGen); 
+        })
+    }
     
-
-        
+    function genNums (){
+        return Math.floor(Math.random()* 250)
     }
 
 
-
 })
+
+
+//generates random number between 1 and 250
+console.log(Math.floor(Math.random()* 250));
